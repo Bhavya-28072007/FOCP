4a078d52bc36038105ce0d334c8674a0579fdc1a
Bhavya-28072007 <mbhavya752@gmail.com>
2025-10-27 20:43:13 +0530

    Create q-9.c

---
+++ FOCP-1 ASSIGMENT/q-9.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main()
+{
+    int number[5],i;
+    for(i=0;i<5;i++)
+    {
+        printf("ENTER NUMBER %d",i);
+        scanf("%d",&number[i]);
+
+    }
+    for(i=0;i<5;i++)
+    {
+        if(number[i]!=99)
+        continue;
+        else if (number[i]==99)
+        {
+            printf("%d index has value of 99 ",i);
+            break;
+        }
+    }
+
+}
